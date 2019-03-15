c2a080b8ac29d9fff7c5d854a1cf346476736464
Joao Alves <aluno19262@ipt.pt>
2019-03-15 19:35:58 +0000

    adiçao do controller Home e da view Index

---
+++ Formulario/Formulario/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Formulario.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            //inicializar um valor para a resposta
+            ViewBag.Resposta = "";
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(string nome, int? idade)
+        {
+            string resposta = "";
+
+            if (nome != "") {
+                //preparar a resposta a enviar para a view
+                resposta = "você chama-se " + nome; 
+            }
+            if (idade != null)
+            {
+               resposta +=" e tem " + idade + " anos.";
+            }
+
+            // enviar a 'resposta' para a view
+            ViewBag.Resposta = resposta;
+
+            return View();
+        }
+    }
+}
